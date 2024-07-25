c842de2a132f8e9683977b7a670d9f65c736be93
Mario Bruno <103866057+mariobrunop@users.noreply.github.com>
2024-07-25 18:29:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,10 @@ function loadData() {
     checkboxes.forEach((checkbox, index) => {
         checkbox.checked = checkboxStates[index] || false;
     });
-    alert('Dados carregados!');
 }
 
 function clearData() {
     localStorage.removeItem('checkboxStates');
     const checkboxes = document.querySelectorAll('.checkbox');
     checkboxes.forEach(checkbox => checkbox.checked = false);
-    alert('Dados limpos!');
 }
